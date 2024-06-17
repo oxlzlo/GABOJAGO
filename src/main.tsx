c084d19fe408d200c7df71b6b1ee3371144f0079
YuHyeonWook <yho7955@naver.com>
2024-06-17 21:29:37 +0900

    chore: 절대 경로 설정함

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -9,4 +10,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </ChakraProvider>
   </React.StrictMode>,
-)
+);
