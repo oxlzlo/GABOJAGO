f9402413e089ccbc32f5105cec3f70a1961632d6
FE_이진영 <159132386+holololob@users.noreply.github.com>
2024-06-18 19:17:22 +0900

    env: svgr 적용

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const Cart = () => {
+  const { cart, total, cleatBasket } = useGlobalContext();
+
+  if (cart.length === 0) {
+    return (
+      <section className="cart">
+        <header>
+          <h1></h1>
+        </header>
+      </section>
+    )
+  }
   return <div>Cart</div>;
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { Box, Heading } from '@chakra-ui/react';
+import Logo from '../assets/logo.svg?react'
 
 const Home = () => {
   return (
     <Box padding="14rem 3rem 7rem">
       <Heading size="3xl">Home</Heading>
+      <Logo />
     </Box>
   );
 };
