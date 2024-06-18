b19c4d8af3f8b1462d1facee08594daa3dbb3485
FE_이진영 <159132386+holololob@users.noreply.github.com>
2024-06-18 18:36:59 +0900

    chore: 충돌 해결

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,3 +1,4 @@
+import Layout from '@/components/layout/Layout';
 import Home from '@/pages/Home';
 import Payment from '@/pages/Payment';
 import ProductDetail from '@/pages/ProductDetail';
@@ -8,7 +9,35 @@ import SignUp from '@/pages/SignUp';
 export const routes = [
   {
     path: '/',
-    element: <Home />,
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+  },
+  {
+    path: '/shopping-basket',
+    element: (
+      <Layout>
+        <Cart />,
+      </Layout>
+    ),
+  },
+  {
+    path: '/products-detail',
+    element: (
+      <Layout>
+        <ProductDetail />,
+      </Layout>
+    ),
+  },
+  {
+    path: '/payment',
+    element: (
+      <Layout>
+        <Payment />
+      </Layout>
+    ),
   },
   {
     path: '/signup',
