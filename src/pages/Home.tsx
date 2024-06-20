86f82e8d7644f7edab7be72b5b87651e20c5ef1b
oxlzlo <yeojin58@naver.com>
2024-06-20 14:18:53 +0900

    refactor: 옵션 검색바 자리 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,20 @@
 import LodgmentList from '@/components/LodgmentList';
 import { Box, Image } from '@chakra-ui/react';
-import Banner from '@/assets/banner.svg?react';
 import SearchBar from '@/components/SearchBar';
 
 const Home = () => {
   return (
-    <Box paddingTop="8rem" position="relative">
-      <Image
-        src="https://tourimage.interpark.com/BBS/Tour/FckUpload/202012/6374243548102999472.jpg"
-        width="100%"
-        height="50vh"
-        objectFit="cover"
-      />
+    <Box paddingTop="8rem">
+      <Box position="relative">
+        <Image
+          src="https://tourimage.interpark.com/BBS/Tour/FckUpload/202012/6374243548102999472.jpg"
+          width="100%"
+          height="50vh"
+          objectFit="cover"
+        />
+        <SearchBar />
+      </Box>
+
       <Box padding="4rem 15rem 7rem">
         <LodgmentList />
       </Box>
