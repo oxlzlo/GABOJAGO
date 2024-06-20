e09fea5da498794df27865a0865f77b2bff83bcf
oxlzlo <yeojin58@naver.com>
2024-06-20 18:11:43 +0900

    design: 옵션 검색바 디자인 수정

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,8 @@
 import { Box, Button, Flex, Input, InputGroup, InputLeftElement, Text, position } from '@chakra-ui/react';
 import emotionStyled from '@emotion/styled';
 import React from 'react';
-import { SearchIcon } from '@chakra-ui/icons';
+import { SearchIcon, CalendarIcon } from '@chakra-ui/icons';
+import People from '../assets/people.svg?react';
 
 const SearchBar = () => {
   return (
@@ -9,15 +10,15 @@ const SearchBar = () => {
       className="searchBar"
       style={{ position: 'absolute', top: '50%', transform: 'translateY(-50%)', right: '0', left: '0' }}>
       <Flex justify="center" align="center">
-        <Box width="130rem" height="6.7rem">
+        <Box width="90.3vw" height="6.5vh">
           <Flex justifyContent="space-between" align="center">
-            <InputGroup width="43rem">
-              <InputLeftElement width="3.6rem" height="3.6rem">
-                <SearchIcon />
+            <InputGroup width="29.9vw">
+              <InputLeftElement width="3vw" height="6.5vh">
+                <SearchIcon w={9} h={9} color="gray" />
               </InputLeftElement>
               <Input
-                height="6.7rem"
-                padding="0 5rem"
+                height="6.5vh"
+                padding="0 5.5rem"
                 border=".1rem solid var(--color-main)"
                 borderRadius=".8rem"
                 backgroundColor="white"
@@ -28,47 +29,88 @@ const SearchBar = () => {
             </InputGroup>
 
             <InputGroup
-              width="40rem"
-              height="6.7rem"
+              width="27.8vw"
+              height="6.5vh"
               border=".1rem solid var(--color-main)"
               borderRadius=".8rem"
               backgroundColor="white">
               <Flex align="center">
-                <Input
-                  width="20rem"
-                  height="5rem"
-                  fontSize="1.6rem"
-                  border="none"
-                  borderRadius="0"
-                  borderRight=".1rem solid var(--color-main)"
-                  type="date"
-                  _focusVisible={{ outline: 'none' }}
-                />
-                <Input
-                  width="20rem"
-                  height="5rem"
-                  fontSize="1.6rem"
-                  border="none"
-                  type="date"
-                  _focusVisible={{ outline: 'none' }}
-                />
+                <InputGroup width="13.5vw">
+                  <InputLeftElement width="3vw" height="5vh">
+                    {/* <CalendarIcon w={8} h={8} color="gray" /> */}
+                  </InputLeftElement>
+                  <Input
+                    height="5vh"
+                    fontSize="1.6rem"
+                    // paddingLeft="3vw"
+                    border="none"
+                    borderRadius="0"
+                    borderRight=".1rem solid var(--color-main)"
+                    type="date"
+                    _focusVisible={{ outline: 'none' }}
+                    // css={{
+                    //   '::-webkit-calendar-picker-indicator': {
+                    //     display: 'none',
+                    //   },
+                    //   '::-webkit-inner-spin-button': {
+                    //     display: 'none',
+                    //   },
+                    //   '::-webkit-clear-button': {
+                    //     display: 'none',
+                    //   },
+                    // }}
+                  />
+                </InputGroup>
+
+                {/* 여기부터 라이브러리 써서 달력 구현 해볼 예정 */}
+                <InputGroup width="13.5vw">
+                  <InputLeftElement width="3vw" height="5vh">
+                    <CalendarIcon w={8} h={8} color="gray" />
+                  </InputLeftElement>
+                  <Input
+                    height="5vh"
+                    fontSize="1.6rem"
+                    paddingLeft="3vw"
+                    border="none"
+                    borderRadius="0"
+                    type="date"
+                    _focusVisible={{ outline: 'none' }}
+                    css={{
+                      '::-webkit-calendar-picker-indicator': {
+                        display: 'none',
+                      },
+                      '::-webkit-inner-spin-button': {
+                        display: 'none',
+                      },
+                      '::-webkit-clear-button': {
+                        display: 'none',
+                      },
+                    }}
+                  />
+                </InputGroup>
               </Flex>
             </InputGroup>
 
-            <Input
-              width="26rem"
-              height="6.7rem"
-              padding="0 5rem"
-              border=".1rem solid var(--color-main)"
-              borderRadius=".8rem"
-              backgroundColor="white"
-              fontSize="1.6rem"
-              placeholder="성인 2명"
-              _focusVisible={{ outline: 'none' }}
-            />
+            <InputGroup width="18.1vw">
+              <InputLeftElement width="3vw" height="6.5vh">
+                <People />
+              </InputLeftElement>
+              <Input
+                width="18.1vw"
+                height="6.5vh"
+                padding="0 3vw"
+                border=".1rem solid var(--color-main)"
+                borderRadius=".8rem"
+                backgroundColor="white"
+                fontSize="1.6rem"
+                placeholder="성인 2명"
+                _focusVisible={{ outline: 'none' }}
+              />
+            </InputGroup>
+
             <Button
-              width="18rem"
-              height="6.7rem"
+              width="12.5vw"
+              height="6.5vh"
               fontSize="3rem"
               borderRadius=".8rem"
               backgroundColor="main"
