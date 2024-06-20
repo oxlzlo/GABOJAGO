f81e5b8811cfd092502b64204d2377c0bc4e955f
oxlzlo <yeojin58@naver.com>
2024-06-20 14:12:19 +0900

    chore: pull main

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,17 @@
 import LodgmentList from '@/components/LodgmentList';
-import { Box } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 import Banner from '@/assets/banner.svg?react';
 import SearchBar from '@/components/SearchBar';
 
 const Home = () => {
   return (
-    <Box paddingTop="8rem">
-      <Box position="relative">
-        <Banner width="100%" height="auto" />
-        <SearchBar />
-      </Box>
+    <Box paddingTop="8rem" position="relative">
+      <Image
+        src="https://tourimage.interpark.com/BBS/Tour/FckUpload/202012/6374243548102999472.jpg"
+        width="100%"
+        height="50vh"
+        objectFit="cover"
+      />
       <Box padding="4rem 15rem 7rem">
         <LodgmentList />
       </Box>
