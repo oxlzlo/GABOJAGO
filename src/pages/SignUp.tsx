0b4ac2444f8c0a2cecd872f012c9d27f96df6498
oxlzlo <yeojin58@naver.com>
2024-06-22 16:50:19 +0900

    feat: 회원가입 post 요청 추가

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../index.css';
 import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import Logo from '../assets/logo.svg?react';
 import emotionStyled from '@emotion/styled';
+import axios from 'axios';
 
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const handleSubmit = async () => {
+    if (password !== confirmPassword) {
+      alert('패스워드가 일치하지 않습니다.');
+      return;
+    }
+
+    const payload = {
+      name,
+      email,
+      password,
+    };
+
+    try {
+      const response = await axios.post(
+        'http://ec2-43-203-40-90.ap-northeast-2.compute.amazonaws.com/open-api/user/register',
+        payload,
+      );
+      if (response.data.result_code === '201') {
+        alert('회원가입이 정상적으로 처리되었습니다.');
+        console.log(name, email, password);
+      }
+    } catch (error) {
+      console.error('회원가입 에러', error);
+      alert('회원가입에 실패하였습니다.');
+    }
+  };
+
   return (
     <Box position="relative" backgroundColor="background" height="100vh">
       <Box
@@ -21,10 +54,20 @@ const SignUp = () => {
           <Logo />
           <Box marginTop="4.9vh">
             <Flex flexDirection="column" justify="center" align="center">
-              <InputBox placeholder="E-mail *" />
-              <InputBox placeholder="Password *" />
-              <InputBox placeholder="Confirm Password *" />
-              <InputBox placeholder="Name *" />
+              <InputBox placeholder="E-mail *" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <InputBox
+                placeholder="Password *"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputBox
+                placeholder="Confirm Password *"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <InputBox placeholder="Name *" value={name} onChange={(e) => setName(e.target.value)} />
               <Button
                 width="27.8vw"
                 height="4.9vh"
@@ -33,6 +76,7 @@ const SignUp = () => {
                 backgroundColor="main"
                 fontSize="1.5rem"
                 color="white"
+                onClick={handleSubmit}
                 _hover={{ bg: 'background', border: '.1rem solid var(--color-main)', color: 'main' }}>
                 Sign up
               </Button>
