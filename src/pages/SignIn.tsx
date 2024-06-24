d0432b379d681501f37455badcb95e18efb0f80c
oxlzlo <yeojin58@naver.com>
2024-06-24 16:30:38 +0900

    feat: 로그인 기능 구현

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../index.css';
 import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import Logo from '../assets/logo.svg?react';
 import emotionStyled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const navigate = useNavigate();
 
   const handleSignupClick = () => {
     navigate('/signup');
+  };
+
+  const handleSubmit = async () => {
+    const payload = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await axios.post(
+        'http://ec2-43-203-40-90.ap-northeast-2.compute.amazonaws.com/open-api/user/login',
+        payload,
+      );
+      if (response.data.result_code === '200') {
+        alert('로그인 되었습니다');
+        navigate('/');
+        console.log(email, password);
+      }
+    } catch (error) {
+      console.error('회원가입 에러', error);
+      alert('로그인에 실패하였습니다.');
+    }
   };
 
   return (
@@ -28,8 +54,13 @@ const SignIn = () => {
           <Logo />
           <Box marginTop="4.9vh">
             <Flex flexDirection="column" justify="center" align="center">
-              <InputBox placeholder="E-mail" />
-              <InputBox placeholder="Password" />
+              <InputBox placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <InputBox
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <Button
                 width="27.8vw"
                 height="4.9vh"
@@ -38,6 +69,7 @@ const SignIn = () => {
                 backgroundColor="main"
                 fontSize="1.5rem"
                 color="white"
+                onClick={handleSubmit}
                 _hover={{ border: '.1rem solid var(--color-main)', bg: 'background', color: 'main' }}>
                 Sign in
               </Button>
