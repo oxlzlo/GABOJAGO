0b786c98efec45a1e2441b9be70a0423cf94d1b9
oxlzlo <yeojin58@naver.com>
2024-06-28 02:38:25 +0900

    feat: 로그아웃 타입 authStore 타입 설정

--- src/lib/types/authStore.ts
+++ src/lib/types/authStore.ts
@@ -6,5 +6,6 @@ export type User = {
 export type AuthState = {
   user: User | null;
   login: (userData: User) => void;
+  logout: (userData: User) => void;
   loadUserFromLocalStorage: () => void;
 };
