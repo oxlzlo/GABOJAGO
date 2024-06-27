8173997613824e14e75b7500d82da6e69d538416
oxlzlo <yeojin58@naver.com>
2024-06-27 12:21:41 +0900

    refactor: 불필요한 Import 제거

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Box, Button, Flex } from '@chakra-ui/react';
 import Logo from '../assets/logo.svg?react';
 import emotionStyled from '@emotion/styled';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import '../index.css';
-import { useAuth } from '../context/AuthContext';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
-  const { login } = useAuth();
   const [name, setName] = useState('');
 
   const navigate = useNavigate();
