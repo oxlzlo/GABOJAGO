0cf39cd099e84395da2e4c9f096e1602868e8f12
oxlzlo <yeojin58@naver.com>
2024-06-28 17:50:54 +0900

    Design: 패스워드 재설정 페이지 디자인 구현

--- src/pages/Resetpw.tsx
+++ src/pages/Resetpw.tsx
@@ -1,7 +1,58 @@
-import { Text } from '@chakra-ui/react';
+import { Box, Flex, Button } from '@chakra-ui/react';
+import Logo from '@/assets/logo.svg?react';
+import emotionStyled from '@emotion/styled';
 
 const Resetpw = () => {
-  return <Text>Resetpw</Text>;
+  return (
+    <Box position="relative" height="100vh" backgroundColor="background">
+      <Box
+        position="absolute"
+        top="0"
+        bottom="0"
+        left="0"
+        right="0"
+        width="41.7vw"
+        height="68.4vh"
+        margin="auto"
+        backgroundColor="white">
+        <Flex flexDirection="column" justify="center" align="center" marginTop="14.6vh">
+          <Logo />
+          <Box marginTop="4.9vh">
+            <Flex flexDirection="column" justify="center" align="center">
+              <InputBox placeholder="E-mail" />
+              <InputBox type="password" placeholder="Password *" />
+              <InputBox type="password" placeholder="Confirm Password *" />
+              <Button
+                width="27.8vw"
+                height="4.9vh"
+                marginTop="2vh"
+                borderRadius=".5rem"
+                backgroundColor="main"
+                fontSize="1.5rem"
+                color="white"
+                _hover={{ border: '.1rem solid var(--color-main)', bg: 'background', color: 'main' }}>
+                Reset Password
+              </Button>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
 };
 
 export default Resetpw;
+
+const InputBox = emotionStyled.input`
+  width : 27.8vw;
+  height: 4.9vh;
+  margin-top: 2vh;
+  padding: 2.6vh 2.1vw;
+  border: .1rem solid var(--color-main);
+  border-radius: .5rem;
+
+  font-size: 1.5rem;
+  color: var(--color-main);
+
+  outline: none;
+`;
