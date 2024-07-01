44841cf4d4ec83be3ef92daeab63574ce672eb50
YuHyeonWook <yho7955@naver.com>
2024-07-01 17:27:55 +0900

    refactor: accommodationList컴포넌트 accommodation 동일한 id 값을 가진 항목을 개별 id 값으로 갖도록 수정

--- src/components/AccommodationList.tsx
+++ src/components/AccommodationList.tsx
@@ -1,11 +1,11 @@
 import { fetchAccommodation } from '@/api';
-import { Accommodation, AccommodationListProps } from '@/lib/types/accommodation';
+import { Accommodation } from '@/lib/types/accommodation';
 import { Box, Flex, Grid, Heading, Image, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
 import { Link } from 'react-router-dom';
 
-const AccommodationList: React.FC<AccommodationListProps> = ({ accommodation }) => {
+const AccommodationList = () => {
   const [accommodations, setAccommodations] = useState<Accommodation[]>([]);
   const [hasMore, setHasMore] = useState<boolean>(true);
   const [page, setPage] = useState<number>(1);
@@ -36,15 +36,11 @@ const AccommodationList: React.FC<AccommodationListProps> = ({ accommodation }) 
   const loadMore = () => {
     fetchAccommodation(page)
       .then((response) => {
-        if (response && Array.isArray(response.data.data.content)) {
-          setAccommodations((prev) => [...prev, ...response.data.data.content]);
-          console.log();
-          setPage(page + 1); // 다음 페이지로 이동함
-          if (response.data.data.content.length === 0) {
-            setHasMore(false);
-          }
-        } else {
-          console.error('Expected an array but got:', response);
+        setAccommodations((prev) => [...prev, ...response.data.data.content]);
+        console.log();
+        setPage(page + 1); // 다음 페이지로 이동함
+        if (response.data.data.content.length === 0) {
+          setHasMore(false);
         }
       })
       .catch((error) => {
@@ -70,58 +66,64 @@ const AccommodationList: React.FC<AccommodationListProps> = ({ accommodation }) 
             desktop: 'repeat(4, 1fr)',
           }}
           gap="1.5rem">
-          {accommodations.map((accommodation) => (
-            <Box
-              key={accommodation.id}
-              width="100%"
-              height="100%"
-              border="1px solid"
-              borderColor="grayLight"
-              borderRadius="0.8rem"
-              gap="1.5rem"
-              _hover={{
-                cursor: 'pointer',
-                transform: 'scale(1.01)',
-                boxShadow: '0 0.5rem 1rem rgba(0, 0, 0, 0.15)',
-              }}>
-              <Link to={`/accommodation/${accommodation.id}`}>
-                <Image
-                  src={accommodation.thumbnail}
-                  alt={accommodation.name}
-                  width="100%"
-                  height="25.7rem"
-                  borderRadius="0.8rem 0.8rem 0 0"
-                />
-                <Box display="flex" flexDirection="column" width="100%" height="auto" paddingLeft="1rem" gap=".5rem">
-                  <Text fontSize="2rem" fontWeight="900" marginTop="1rem">
-                    {accommodation.name}
-                  </Text>
-                  <Box padding=".5rem">
-                    <Text fontSize="1.5rem" color="gray">
-                      {accommodation.address}
+          {accommodations.map((accommodation, index) => (
+            <Box key={`${accommodation.id}-${index}`}>
+              <Box
+                width="100%"
+                height="100%"
+                border="1px solid"
+                borderColor="grayLight"
+                borderRadius="0.8rem"
+                gap="1.5rem"
+                _hover={{
+                  cursor: 'pointer',
+                  transform: 'scale(1.01)',
+                  boxShadow: '0 0.5rem 1rem rgba(0, 0, 0, 0.15)',
+                }}>
+                <Link to={`/accommodation/${accommodation.id}`}>
+                  <Image
+                    src={accommodation.thumbnail}
+                    alt={accommodation.name}
+                    width="100%"
+                    height="25.7rem"
+                    borderRadius="0.8rem 0.8rem 0 0"
+                  />
+                  <Box display="flex" flexDirection="column" width="100%" height="auto" paddingLeft="1rem" gap=".5rem">
+                    <Text fontSize="2rem" fontWeight="900" marginTop="1rem">
+                      {accommodation.name}
                     </Text>
-                    <Text fontSize="1.5rem" color="gray">
-                      {accommodation.numbers}
-                    </Text>
-                    <Text fontSize="1.8rem" color="gray">
-                      {accommodation.comment}
-                    </Text>
-                  </Box>
-                  <Box display="flex" flexDirection="column" alignItems="flex-end" marginTop="3rem" paddingRight="1rem">
-                    <Text fontSize="1.5rem" color="gray" paddingRight="2.8rem">
-                      1박당 요금
-                    </Text>
-                    <Flex flexDirection="column">
-                      <Text fontSize="2rem" color="red">
-                        {`${accommodation.price.toLocaleString('ko-KR', {
-                          style: 'decimal',
-                          currency: 'KRW',
-                        })}원`}
+                    <Box padding=".5rem">
+                      <Text fontSize="1.5rem" color="gray">
+                        {accommodation.address}
                       </Text>
-                    </Flex>
+                      <Text fontSize="1.5rem" color="gray">
+                        {accommodation.numbers}
+                      </Text>
+                      <Text fontSize="1.8rem" color="gray">
+                        {accommodation.comment}
+                      </Text>
+                    </Box>
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-end"
+                      marginTop="3rem"
+                      paddingRight="1rem">
+                      <Text fontSize="1.5rem" color="gray" paddingRight="2.8rem">
+                        1박당 요금
+                      </Text>
+                      <Flex flexDirection="column">
+                        <Text fontSize="2rem" color="red">
+                          {`${accommodation.price.toLocaleString('ko-KR', {
+                            style: 'decimal',
+                            currency: 'KRW',
+                          })}원`}
+                        </Text>
+                      </Flex>
+                    </Box>
                   </Box>
-                </Box>
-              </Link>
+                </Link>
+              </Box>
             </Box>
           ))}
         </Grid>
