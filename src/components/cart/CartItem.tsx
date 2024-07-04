1cdd725d7c76467ca2be848c5d68af03aac2d426
YuHyeonWook <yho7955@naver.com>
2024-07-05 07:37:19 +0900

    design: 장바구니 없을 경우 장바구니 비어있다고 스타일 작성

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -4,6 +4,7 @@ import { Box, Flex, Image, Text, Divider, Button } from '@chakra-ui/react';
 import { Rooms } from '@/lib/types/accommodation';
 import { CartCheckbox } from '@/lib/common/CartCheckbox';
 import { CloseIcon } from '@chakra-ui/icons';
+import { useNavigate } from 'react-router-dom';
 
 type CartItems = {
   cart_item_id: number;
@@ -14,6 +15,7 @@ type CartItems = {
 
 const CartItem = ({ onSelecRooms }) => {
   const [cartRooms, setCartRooms] = useState<CartItems[]>([]); // 장바구니 추가한 객실은 해당 state에 담김
+  const navigate = useNavigate();
 
   /**
    * 장바구니에 담긴 상품 조회
@@ -40,64 +42,91 @@ const CartItem = ({ onSelecRooms }) => {
     }
   };
 
+  const handleAccommdationList = () => {
+    navigate('/');
+  };
+
   return (
     <>
-      {cartRooms.map((cartRoom, _) => (
-        <Box
-          key={cartRoom.cart_item_id}
-          padding="1.5rem"
-          marginBottom="1rem"
-          border="1px solid"
-          borderColor="grayLight"
-          borderRadius=".5rem">
-          <Flex justifyContent="end">
-            <Button onClick={() => handleDeleteCartRoom(cartRoom.cart_item_id)}>
-              <CloseIcon />
-            </Button>
-          </Flex>
-          <Flex marginBottom="2rem" alignItems="center" gap="1rem">
-            <Box marginBottom="8rem">
-              <CartCheckbox cartRoom={cartRoom} onSelecRooms={onSelecRooms} colorScheme="" borderColor="main" />
-            </Box>
-            <Image
-              src={cartRoom.room.imageList}
-              alt={cartRoom.room.roomTypeName}
-              width="10rem"
-              height="10rem"
-              marginRight="2rem"
-              border="1px solid"
-              borderColor="grayLight"
-              borderRadius=".5rem"
-            />
-            <Box>
-              <Text fontSize="2rem" fontWeight="900">
-                {cartRoom.room.roomTypeName}
-              </Text>
-              <Text fontSize="1.5rem">{cartRoom.room.roomType}</Text>
-              {/* <Text fontSize="1.2rem">
+      {cartRooms.length === 0 ? (
+        <Flex flexDirection="column">
+          <Text fontSize="2.5rem" fontWeight="900" color="gray" textAlign="center" marginTop="5rem">
+            장바구니가 비어 있습니다.
+          </Text>
+          <Button
+            onClick={handleAccommdationList}
+            width="12rem"
+            height="5rem"
+            borderRadius=".5rem"
+            marginTop="2rem"
+            alignSelf="center"
+            background="main"
+            color="white"
+            fontSize="1.5rem"
+            _hover={{
+              background: 'primaryHover',
+            }}>
+            숙박리스트 보기
+          </Button>
+        </Flex>
+      ) : (
+        cartRooms.map((cartRoom, _) => (
+          <Box
+            key={cartRoom.cart_item_id}
+            padding="1.5rem"
+            marginBottom="1rem"
+            border="1px solid"
+            borderColor="grayLight"
+            borderRadius=".5rem">
+            <Flex justifyContent="end">
+              <Button onClick={() => handleDeleteCartRoom(cartRoom.cart_item_id)}>
+                <CloseIcon />
+              </Button>
+            </Flex>
+            <Flex marginBottom="2rem" alignItems="center" gap="1rem">
+              <Box marginBottom="8rem">
+                <CartCheckbox cartRoom={cartRoom} onSelecRooms={onSelecRooms} colorScheme="" borderColor="main" />
+              </Box>
+              <Image
+                src={cartRoom.room.imageList}
+                alt={cartRoom.room.roomTypeName}
+                width="10rem"
+                height="10rem"
+                marginRight="2rem"
+                border="1px solid"
+                borderColor="grayLight"
+                borderRadius=".5rem"
+              />
+              <Box>
+                <Text fontSize="2rem" fontWeight="900">
+                  {cartRoom.room.roomTypeName}
+                </Text>
+                <Text fontSize="1.5rem">{cartRoom.room.roomType}</Text>
+                {/* <Text fontSize="1.2rem">
                 평점: {cartItem.rating} / 리뷰 수: {cartItem.reviewCount}
               </Text> */}
-            </Box>
-          </Flex>
-          <Divider borderColor="main" />
-          <Box marginTop="2rem" fontSize="1.2rem">
-            <Text fontSize="1.3rem">
-              이용기간: {cartRoom.start_date} - {cartRoom.end_date}
-            </Text>
-            <Text fontSize="1.3rem">이용자 수: {cartRoom.room.roomDefaultGuest}인</Text>
-            <Flex justifyContent="flex-end" fontSize="2rem" fontWeight="700">
-              <Text>
-                <span style={{ color: 'red' }}>
-                  {`${cartRoom.room.roomPrice.toLocaleString('ko-KR', {
-                    style: 'decimal',
-                    currency: 'KRW',
-                  })}원`}
-                </span>
-              </Text>
+              </Box>
             </Flex>
+            <Divider borderColor="main" />
+            <Box marginTop="2rem" fontSize="1.2rem">
+              <Text fontSize="1.3rem">
+                이용기간: {cartRoom.start_date} - {cartRoom.end_date}
+              </Text>
+              <Text fontSize="1.3rem">이용자 수: {cartRoom.room.roomDefaultGuest}인</Text>
+              <Flex justifyContent="flex-end" fontSize="2rem" fontWeight="700">
+                <Text>
+                  <span style={{ color: 'red' }}>
+                    {`${cartRoom.room.roomPrice.toLocaleString('ko-KR', {
+                      style: 'decimal',
+                      currency: 'KRW',
+                    })}원`}
+                  </span>
+                </Text>
+              </Flex>
+            </Box>
           </Box>
-        </Box>
-      ))}
+        ))
+      )}
     </>
   );
 };
