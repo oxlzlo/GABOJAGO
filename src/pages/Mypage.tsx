c92f8c68c25d1e44bc251abbccddef788344df5a
oxlzlo <yeojin58@naver.com>
2024-07-06 21:23:56 +0900

    design: imgUrl이 없을 때 display: none 적용

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -181,6 +181,7 @@ const Mypage = () => {
                 className="profile_img"
                 alt="Profile"
                 style={{
+                  display: imgUrl ? 'block' : 'none',
                   position: 'absolute',
                   top: '50%',
                   transform: 'translateY(-50%)',
