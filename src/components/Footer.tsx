00bc180b67efe9b18534b3cf8f519decfc393fd1
YuHyeonWook <yho7955@naver.com>
2024-07-06 12:13:20 +0900

    refactor: footer 컴포넌트 스타일 수정 및 불필요한 색상 변수 제거

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,25 +3,29 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <Box padding="3rem" backgroundColor="garyDark">
-      <Flex justifyContent="center" alignItems="center">
+    <Box padding="2.5rem" backgroundColor="garyDark">
+      <Flex flexDirection="column" justifyContent="center" alignItems="center">
+        <Box display="flex" justifyContent="center" alignItems="center" padding="1rem">
+          <Text fontSize="3rem" color="main" fontWeight="900">
+            GABOJAGO 플랫폼
+          </Text>
+        </Box>
         <Text fontSize="1.8rem" color="white">
-          <Box display="flex" justifyContent="center" alignItems="center" padding="1rem">
-            <Text fontSize="3rem" color="main" fontWeight="900">
-              GABOJAGO 플랫폼
-            </Text>
-          </Box>
           온라인 여행 및 관련 서비스 분야이고, 사용자와 거리를 좁히고 친근하게 다가갑니다.
-          <Box display="flex" justifyContent="center" padding="1rem">
-            <Box
+        </Text>
+        <Box display="flex" justifyContent="center" padding="1rem">
+          <Link to="https://github.com/your-username/your-repository">
+            <Text
+              fontSize="1.8rem"
+              color="white"
               _hover={{
                 color: 'main',
                 textDecoration: 'underline',
               }}>
-              <Link to="https://github.com/your-username/your-repository">GitHub 레포지토리 방문하기</Link>
-            </Box>
-          </Box>
-        </Text>
+              GitHub 레포지토리 방문하기
+            </Text>
+          </Link>
+        </Box>
       </Flex>
     </Box>
   );
