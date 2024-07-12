d2080e9e587ff8b85f63f39bd714a9fe59799c9f
YuHyeonWook <yho7955@naver.com>
2024-07-12 19:41:34 +0900

    design: 색상변수 theme파일에서 관리하도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '@/styles/customTheme.ts';
+import '@/styles/theme.css';
 import './index.css';
 
 async function enableMocking() {
