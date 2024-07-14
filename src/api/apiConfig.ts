6182ae00aabdfb3b766214a592cf4acc1ff14499
YuHyeonWook <yho7955@naver.com>
2024-07-14 10:36:01 +0900

    refactor: axios interceptor 응답 설정 - 로그인 토큰이 만료되었을때 로그인 페이지 이동하게끔 수정

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -7,6 +7,11 @@ const client = axios.create({
   baseURL: 'http://ec2-43-203-40-90.ap-northeast-2.compute.amazonaws.com/',
 });
 
+export default client;
+
+/**
+ * 요청 인터셉터 설정
+ */
 client.interceptors.request.use(
   (config) => {
     // 로컬 스토리지에서 토큰을 반환
@@ -22,4 +27,19 @@ client.interceptors.request.use(
   },
 );
 
-export default client;
+/**
+ * 응답 인터셉터 설정
+ * 로그인 토큰이 만료되었을 때, 로그인 페이지로 이동
+ */
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // 토큰 만료 시 로그인 페이지로 이동
+      window.location.href = '/signin';
+    }
+    return Promise.reject(error);
+  },
+);
