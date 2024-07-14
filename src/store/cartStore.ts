63a70328a5e4460d9e278a4a6a37073f61fa67f7
YuHyeonWook <yho7955@naver.com>
2024-07-14 10:37:22 +0900

    refactor: 불필요한 주석 삭제

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -6,5 +6,4 @@ export const useCartStore = create<CartStore>((set) => ({
   addToCart: (room) => set((state) => ({ cartRooms: [...state.cartRooms, room] })),
   removeCart: (cartItemId) =>
     set((state) => ({ cartRooms: state.cartRooms.filter((cartRoom) => cartRoom.cart_item_id !== cartItemId) })),
-  //   clearCart: () => set({ cartRooms: [] }),
 }));
