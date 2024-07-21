c9f89bd154d069f81889a0ec25db99e85ef5a550
YuHyeonWook <yho7955@naver.com>
2024-07-21 09:25:04 +0900

    feat: 텍스트 압축

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    viteCompression({
+      // 압축 알고리즘 지정, 기본적으로는 'gzip'을 사용
+      algorithm: 'gzip',
+      // 압축된 파일의 확장자를 '.gz'로 설정
+      ext: '.gz',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
